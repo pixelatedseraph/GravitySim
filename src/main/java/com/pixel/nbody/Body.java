e5597f799c439b3d4f5b0d7d2b27549d89bf9bc1
pixelatedseraph <janujani819@gmail.com>
2025-09-14 22:35:24 +0530

    New Body

--- src/main/java/com/pixel/nbody/Body.java
+++ src/main/java/com/pixel/nbody/Body.java
@@ -1,10 +1,17 @@
 package com.pixel.nbody;
 
 public class Body {
-    private Vector3D position, velocity,acceleration;
-    private double mass,radius;
-    int bodyID;
-
+    private Vector3D position, velocity ,acceleration;
+    private final double mass;
+    private final double radius;
+    private final int bodyID;
+        /* basic constructor */
+        Body(double mass,double radius, int bodyID){
+            this.mass = mass;
+            this.radius = radius;
+            this.bodyID = bodyID;
+            this.acceleration  = new Vector3D(0,0,0);
+        }
       /* getter methods for the fields above */
 
      /* returns current position of body */
@@ -23,18 +30,36 @@ public class Body {
     public double getMass(){
        return mass;
       }
-    /* return mass of body */
-    /*setter methods for this class fields */
+    public double getRadius(){
+        return radius;
+    }
+      public int getBodyID(){
+        return bodyID;
+      }
 
+    /*setter methods for this class fields */
     public void setPosition(Vector3D position){
         this.position = position;
       }
 
-    public void setVelocity(Vector3D velocity){
-        this.velocity = velocity;
-      }
-    public void setAcceleration(Vector3D acceleration){
-        this.acceleration = acceleration;
-      }
+        public void setVelocity(Vector3D velocity){
+            this.velocity = velocity;
+        }
+        public void setAcceleration(Vector3D acceleration){
+            this.acceleration = acceleration;
+        }
+        /* method to update position and velocity - this will be called by RK4 integrator */
+        public void updatePosAndVel(Vector3D npos , Vector3D nvel){
+            setPosition(npos);
+            setVelocity(nvel);
+        }
+        /*method to calculate distance between to bodies */
+        public double distanceBtw(Body Other){
+            return this.position.distance(Other.position);
+        }
+        /* kinetic energy */
+        public double KineticEnergy(){
+            return ( 0.5 * mass * velocity.magnitude() );
+        }
 
 }
